f080add9d66d4b2d9b68673e4d806bd7536d1988
Alan Gilbert <alanhgilbert@gmail.com>
2015-10-25 20:46:26 +0200

    basic working version

---
+++ src/Main.hs
@@ -0,0 +1,12 @@
+module Main where
+
+import System.Environment (getArgs)
+
+main :: IO ()
+main = do
+  args <- getArgs
+  let width = if (length args == 0) then 80 else (read $ args !! 0)
+  let dataSource = if (length args <= 1) then getContents else readFile (args !! 1)
+  content <- map (take width) . lines <$> dataSource
+  mapM_ putStrLn content
+
